23743edf4a590d5faa4958dcdfa39e4cd4149e7a
Jordyfel <jord_id@abv.bg>
2026-07-09 00:02:58 +0300

    Add reordering

--- addons/procedural_texture/editor/procedural_texture_editor.gd
+++ addons/procedural_texture/editor/procedural_texture_editor.gd
@@ -10,6 +10,10 @@ var texture: ProceduralTexture
 var dragging:= false
 
 
+func _ready() -> void:
+	tree.set_drag_forwarding(_tree_get_drag_data, _tree_can_drop_data, _tree_drop_data)
+
+
 func edit(procedural_texture: ProceduralTexture) -> void:
 	if procedural_texture == texture:
 		return
@@ -81,3 +85,58 @@ func _on_remove_button_pressed() -> void:
 	EditorInterface.edit_resource(texture)
 	EditorInterface.set_object_edited(texture, true)
 	selected.free()
+
+
+func _tree_get_drag_data(_at_position: Vector2) -> Variant:
+	var selected:= tree.get_selected()
+	if selected == null:
+		return null
+
+	var node:= texture.root_node.children[selected.get_index()]
+	var preview:= Label.new()
+	preview.text = node._get_name()
+	tree.set_drag_preview(preview)
+	return node
+
+
+func _tree_can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	var item:= tree.get_item_at_position(at_position)
+	if item == null:
+		return false
+
+	var node:= data as TextureNode
+	if node == null:
+		return false
+
+	tree.drop_mode_flags = Tree.DROP_MODE_INBETWEEN
+	return true
+
+
+func _tree_drop_data(at_position: Vector2, data: Variant) -> void:
+	var node:= data as TextureNode
+	var item:= tree.get_item_at_position(at_position)
+	if item == null:
+		return
+
+	var index:= texture.root_node.children.find(node)
+	var drop_section:= tree.get_drop_section_at_position(at_position)
+	match drop_section:
+		-100:
+			return
+		-1:
+			tree.get_root().get_child(index).move_before(item)
+			_move_node(node, item.get_index() - 1)
+		0:
+			pass # TODO: Last child.
+		1:
+
+			tree.get_root().get_child(index).move_after(item)
+			_move_node(node, item.get_index() + 1)
+		2:
+			pass # TODO: First child.
+
+
+func _move_node(node: TextureNode, to_index: int) -> void:
+	texture.root_node.children.erase(node)
+	texture.root_node.children.insert(to_index, node)
+	texture.update()
