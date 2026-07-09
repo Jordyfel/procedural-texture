e773af689a6dd3d6c1567503696e819ae3d3cdf5
Jordyfel <jord_id@abv.bg>
2026-07-09 10:32:36 +0300

    Add rect shape boilerplate

---
+++ addons/procedural_texture/shapes/rect_shape.gd
@@ -0,0 +1,28 @@
+@tool
+class_name RectShape
+extends TextureNodeShape
+
+@export var rect: Vector2:
+	set(value):
+		rect = value
+		emit_changed()
+
+func _set_material_parameters() -> void:
+	super()
+	RenderingServer.material_set_param(material, "rect", rect)
+
+
+func _get_name() -> String:
+	return "Rectangle"
+
+
+func _get_height() -> int:
+	return ceil(max(rect.x, rect.y))
+
+
+func _get_width() -> int:
+	return ceil(max(rect.x, rect.y))
+
+
+func _get_shader() -> Shader:
+	return load("uid://cfkqxw1t0o11m")
